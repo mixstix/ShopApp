22533bd031a117e3aa65ea27a24f10c7283c3f11
mixstix <mixstixx@gmail.com>
2015-03-20 13:08:50 +0800

    Got the delete button to work for all items

--- js/app.js
+++ js/app.js
@@ -10,12 +10,28 @@ $(document).ready(function() {
     }
     
     $('#add').click(function() {
-        var itemHtml = '<div class="item-row">' + '<div class="unticked"></div>' + '<div class="item">' + $('input#add-item').val() + '</div>' + '<button id="delete">DEL</button>' + '<hr class="item-separator">' + '</div>';
+        var itemHtml = '<div class="item-row">' + '<div class="unticked"></div>' + '<div class="item">' + $('input#add-item').val() + '</div>' + '<button class="delete">DEL</button>' + '<hr class="item-separator">' + '</div>';
         $('.list').append(itemHtml);
         $('input#add-item').val("");
         ticked();
+        deleteBtn();
         return false;
     });
+    
+//    function addItem() {
+//	var newItem = $('input#add-item').val("");
+//	if(newItem.trim().length === 0) {
+//		alert("You must enter an item to be added.");
+//        return;
+//	}
+    
+    
+    
+    function deleteBtn() {
+        $('.delete').click(function() {
+            $(this).closest('.item-row').remove();
+        });
+    }
     
 
     
